082ae3ccaec1879a37de33fda2dee0ec37bbd6ae
Arpit Bansal <abansal3@babson.edu>
2016-10-01 16:29:58 -0400

    mysql

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+// Twilio
 var accountSid = 'AC2705780d0aef67e3ae69030ef529ec37'; // Your Account SID from www.twilio.com/console
 var authToken = '21728edeb603084b5beda48e38c83db4';   // Your Auth Token from www.twilio.com/console
-
 var twilio = require('twilio');
 var client = new twilio.RestClient(accountSid, authToken);
 
-
 // Database connection
-
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -31,10 +29,7 @@ connection.end();
 }, function(err, message) {
     console.log(message);
 });
-
-client.messages("SM3bcd8e5f1a3074e265cc8d307257dc8d").get(function(err, message) {
-    console.log(message);
-});*/
+*/
 
 function parse_message(message) {
     var medicines = message.body.split(',');
