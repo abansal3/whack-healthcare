2bfb00bcf3ce119e127f750660100f2dac703e84
Dorothysun <daxisun@hotmail.com>
2016-10-01 21:34:45 -0400

    dashboard

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,21 @@
 // Module
 
-var medrelay = angular.module('medrelay',['ngRoute','ngResource']);
+var medrelay = angular.module('medrelay',['ngResource']);
 
  
-medrelay.controller('mainController',['$scope', '$resource', function($scope,$resource) {
+medrelay.controller('mainController',['$scope', '$http', function($scope,$http) {
 
-   $scope.data = $resource("url here", 
-   {
-     get: {
-         method: "JSONP"
-     }
-   });
 
-   $scope.dataresult = $scope.data.get({ /* json parsed */ });
-
+  // Simple GET request example:
+/*$http({
+  method: 'GET',
+  url: 'https://medrelay.herokuapp.com/data/'
+}).then(function successCallback(response) {
+  console.log('Got response: ', response);
+    $scope.name = response.name;
+  }, function errorCallback(response) {
+  console.log('Got error: ', response);
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });*/
 }]);
